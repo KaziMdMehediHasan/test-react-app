b9a413986c2fa8fbf176d37df325cd3975a6d66a
smsakawat <sakawat602872@gmail.com>
2022-01-01 17:28:44 +0600

    contact added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About/About ";
+import Contact from "./components/Contact/Contact";
 import Home from "./components/Home";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Home></Home>
       <About />
+      <Contact />
     </div>
   );
 }

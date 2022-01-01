4178524657ce2cc2a79d8545bfa76e1822cf8a8b
KaziMdMehediHasan <=>
2022-01-01 17:14:16 +0600

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is home component</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, exercitationem neque maiores porro tempora vero architecto ad deserunt, harum quae eligendi ex. Debitis autem tenetur sint non excepturi, reiciendis, dolores adipisci officia quo quod fugiat temporibus velit natus? Ratione, fugit.</p>
+        </div>
+    );
+};
+
+export default Home;
